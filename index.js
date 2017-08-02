a752321423f448d7c88a56e3fd2bf7f635d0652a
NicolaFerracin <nicola.ferracin@gmail.com>
2017-08-02 23:21:57 +0200

    Add static css dir access and remove routes.js from server

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
+const cacheTime = 60*60*24*1000*7; // 7 days
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+
+app.use('/css', express.static(__dirname + '/css', { maxAge: cacheTime }));
 
 const Poet = require('poet');
 const poet = Poet(app, {
@@ -14,8 +17,6 @@ const poet = Poet(app, {
 });
 require('./routes/poet.js')(poet);
 poet.watch().init();
-
-require('./routes/routes.js')(app);
 
 app.listen(port, () => {
   console.log('App listening on port', port);
