2fb8b99a1986f142ea0e1a47b79a048c13c92350
NicolaFerracin <nicola.ferracin@gmail.com>
2017-08-03 22:23:11 +0200

    Fix poet routing

--- routes/poet.js
+++ routes/poet.js
@@ -1,16 +1,11 @@
 module.exports = (poet) => {
     poet.addRoute('/', (req, res, next) => {
         const posts = poet.helpers.getPosts();
-        res.render('blog', { posts: posts, title: 'Blog' });
+        res.render('blog', { posts, title: 'Blog' });
     });
 
-    poet.addRoute('/blog/post/:post', (req, res, next) => {
-        const post = poet.helpers.getPost(req.params.post);
-        res.render('index', { post: post, page: 'blog' });
-    });
-
-    poet.addRoute('/blog/tag/:tag', (req, res, next) => {
+    poet.addRoute('/tag/:tag', (req, res, next) => {
         const posts = poet.helpers.postsWithTag(req.params.tag);
-        res.render('blog', { posts: posts, page: 'blog', title: `Posts with tag: ${req.params.tag}` });
+        res.render('blog', { posts, title: `Posts with tag:</br>${req.params.tag}` });
     });
 };
