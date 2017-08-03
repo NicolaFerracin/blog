4cea38a6c46cab9abec158bcc77735e4c0ce423f
NicolaFerracin <nicola.ferracin@gmail.com>
2017-08-03 22:58:26 +0200

    Remove moment.js

--- helpers.js
+++ helpers.js
@@ -1,9 +1,3 @@
-const moment = require('moment');
-
-exports.moment = moment;
-
-exports.dump = (obj) => JSON.stringify(obj, null, 2);
-
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 exports.prettifyDate = (rawDate) => {
@@ -11,3 +5,8 @@ exports.prettifyDate = (rawDate) => {
     return `${date.getDate()} ${months[date.getMonth()].substring(0, 3)} ${date.getFullYear()}`;
 };
 
+exports.isFutureDate = (date) => {
+    const now = new Date();
+    const toCompare = new Date(date);
+    return toCompare > now;
+};
