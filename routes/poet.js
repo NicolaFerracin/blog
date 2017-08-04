6a940c28bb17c4b4481e9aca63d22f15bcc00024
NicolaFerracin <nicola.ferracin@gmail.com>
2017-08-04 23:57:38 +0200

    Remove line break

--- routes/poet.js
+++ routes/poet.js
@@ -6,6 +6,6 @@ module.exports = (poet) => {
 
     poet.addRoute('/tag/:tag', (req, res, next) => {
         const posts = poet.helpers.postsWithTag(req.params.tag);
-        res.render('blog', { posts, title: `Posts with tag:</br>${req.params.tag}` });
+        res.render('blog', { posts, title: `Posts with tag: ${req.params.tag}` });
     });
 };
